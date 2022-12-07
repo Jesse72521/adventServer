b5b39aa5131882446949baedd0e052a2858605ae
jesseforeup <jesse@foreup.com>
2022-12-07 13:15:07 -0700

    update frontend package

--- frontend/js/tastings.js
+++ frontend/js/tastings.js
@@ -3,126 +3,182 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Sidenav.init(elems, {});
 });
 
+// change this is developing locally
+// this.baseURL = "http://localhost:3006";
+this.baseURL = "https://advent-calendar.herokuapp.com";
+
 document.getElementById("jesseLogin").addEventListener("click", () => {
-  // let url = "http://localhost:3006/getTasting";
-  // let params = `uniqueCode=${uniqueResults}`;
-  // var xmlHttp = new XMLHttpRequest();
-  // xmlHttp.open("GET", url + "?" + params, false);
-  // xmlHttp.onreadystatechange = function () {
-  //   let response = JSON.parse(xmlHttp.responseText);
-  //   if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-  //     document.getElementById("textarea1label").innerText +=
-  //       response.response.first_label;
-  //     document.getElementById("textarea2label").innerText +=
-  //       response.response.second_label;
-  //     document.getElementById("submit-tasting").innerText +=
-  //       response.response.first_label + response.second_label;
-  //   }
-  // };
-
-  // xmlHttp.send(null);
-
-  console.log("jesseClicked");
-
+  clearTastingColumn();
+  renderTastingColumn();
+  document.getElementById("jesseLogin").classList.add("darken-2");
+  document.getElementById("zackLogin").classList.remove("darken-2");
+  document.getElementById("westonLogin").classList.remove("darken-2");
+  this.selected = "Jesse";
+  let url = this.baseURL + "/getAllTastingsByName";
+  let params = `name=Jesse`;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url + "?" + params, false);
+  xmlHttp.onreadystatechange = function () {
+    let response = JSON.parse(xmlHttp.responseText);
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      Object.values(response.response).forEach((val) => {
+        notesArea = document.getElementById("notes" + val.day);
+        notesArea.innerText = val.notes;
+        M.textareaAutoResize(notesArea);
+        guessesArea = document.getElementById("guesses" + val.day);
+        guessesArea.innerText = val.guesses;
+        M.textareaAutoResize(guessesArea);
+        ratingArea = document.getElementById("rating" + val.day);
+        ratingArea.innerText = val.rating;
+      });
+    }
+  };
+  xmlHttp.send(null);
   document.getElementById("tasting-area").classList.remove("hidden");
 });
 
 document.getElementById("westonLogin").addEventListener("click", () => {
-  // let url = "http://localhost:3006/getTasting";
-  // let params = `uniqueCode=${uniqueResults}`;
-  // var xmlHttp = new XMLHttpRequest();
-  // xmlHttp.open("GET", url + "?" + params, false);
-  // xmlHttp.onreadystatechange = function () {
-  //   let response = JSON.parse(xmlHttp.responseText);
-  //   if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-  //     document.getElementById("textarea1label").innerText +=
-  //       response.response.first_label;
-  //     document.getElementById("textarea2label").innerText +=
-  //       response.response.second_label;
-  //     document.getElementById("submit-tasting").innerText +=
-  //       response.response.first_label + response.second_label;
-  //   }
-  // };
-
-  // xmlHttp.send(null);
-
-  console.log("westonClicked");
-
+  clearTastingColumn();
+  renderTastingColumn();
+  document.getElementById("westonLogin").classList.add("darken-2");
+  document.getElementById("zackLogin").classList.remove("darken-2");
+  document.getElementById("jesseLogin").classList.remove("darken-2");
+  this.selected = "Weston";
+  let url = this.baseURL + "/getAllTastingsByName";
+  let params = `name=Weston`;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url + "?" + params, false);
+  xmlHttp.onreadystatechange = function () {
+    let response = JSON.parse(xmlHttp.responseText);
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      if (response.response.length > 0) {
+        Object.values(response.response).forEach((val) => {
+          notesArea = document.getElementById("notes" + val.day);
+          notesArea.innerText = val.notes;
+          M.textareaAutoResize(notesArea);
+          guessesArea = document.getElementById("guesses" + val.day);
+          guessesArea.innerText = val.guesses;
+          M.textareaAutoResize(guessesArea);
+          ratingArea = document.getElementById("rating" + val.day);
+          ratingArea.innerText = val.rating;
+        });
+      }
+    }
+  };
+  xmlHttp.send(null);
   document.getElementById("tasting-area").classList.remove("hidden");
 });
 
 document.getElementById("zackLogin").addEventListener("click", () => {
-  // let url = "http://localhost:3006/getTasting";
-  // let params = `uniqueCode=${uniqueResults}`;
-  // var xmlHttp = new XMLHttpRequest();
-  // xmlHttp.open("GET", url + "?" + params, false);
-  // xmlHttp.onreadystatechange = function () {
-  //   let response = JSON.parse(xmlHttp.responseText);
-  //   if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-  //     document.getElementById("textarea1label").innerText +=
-  //       response.response.first_label;
-  //     document.getElementById("textarea2label").innerText +=
-  //       response.response.second_label;
-  //     document.getElementById("submit-tasting").innerText +=
-  //       response.response.first_label + response.second_label;
-  //   }
-  // };
-
-  // xmlHttp.send(null);
-
-  console.log("zackClicked");
-
+  clearTastingColumn();
+  renderTastingColumn();
+  document.getElementById("zackLogin").classList.add("darken-2");
+  document.getElementById("jesseLogin").classList.remove("darken-2");
+  document.getElementById("westonLogin").classList.remove("darken-2");
+  this.selected = "Zack";
+  let url = this.baseURL + "/getAllTastingsByName";
+  let params = `name=Zack`;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url + "?" + params, false);
+  xmlHttp.onreadystatechange = function () {
+    let response = JSON.parse(xmlHttp.responseText);
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      Object.values(response.response).forEach((val) => {
+        notesArea = document.getElementById("notes" + val.day);
+        notesArea.innerText = val.notes;
+        M.textareaAutoResize(notesArea);
+        guessesArea = document.getElementById("guesses" + val.day);
+        guessesArea.innerText = val.guesses;
+        M.textareaAutoResize(guessesArea);
+        ratingArea = document.getElementById("rating" + val.day);
+        ratingArea.innerText = val.rating;
+      });
+    }
+  };
+  xmlHttp.send(null);
   document.getElementById("tasting-area").classList.remove("hidden");
 });
 
-function submitTasting(
-  day1,
-  day2,
-  day3,
-  day4,
-  day5,
-  day6,
-  day7,
-  day8,
-  day9,
-  day10,
-  day11,
-  day12
-) {
-  console.log("clicked");
-  //   let url = "http://localhost:3006/newTasting";
-  //   var xmlHttp = new XMLHttpRequest();
-  //   xmlHttp.open("POST", url, false);
-  //   xmlHttp.setRequestHeader("Content-type", "application/json");
-  //   xmlHttp.onreadystatechange = function () {
-  //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-  //       alert("Tasting submitted succesfully");
-  //     }
-  //   };
-  //   let params = {
-  //     firstLabel: firstLabel,
-  //     firstAnswer: firstAnswer,
-  //     secondLabel: secondLabel,
-  //     secondAnswer: secondAnswer,
-  //     recipientName: recipientName,
-  //     uniqueCode: uniqueCode,
-  //   };
-  //   xmlHttp.send(JSON.stringify(params));
+function clearTastingColumn() {
+  document.getElementById("tasting-area-column").innerHTML = "";
+}
+
+function renderTastingColumn() {
+  for (var i = 1; i < 13; i++) {
+    document.getElementById("tasting-area-column").innerHTML += `
+            <div class="card horizontal">
+              <div class="card-stacked">
+                <div class="card-content">
+                  <textarea
+                    id="notes${i}"
+                    class="materialize-textarea tastingNotes"
+                    placeholder="Day ${i} Notes"
+                  ></textarea>
+
+                  <textarea
+                    id="guesses${i}"
+                    class="materialize-textarea tastingGuess"
+                    placeholder="Guesses"
+                  ></textarea>
+
+                  <textarea
+                    id="rating${i}"
+                    class="materialize-textarea tastingRating"
+                    placeholder="Rating"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            `;
+  }
+}
+
+function submitTastings(days) {
+  let url = this.baseURL + "/saveTastings";
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", url, false);
+  xmlHttp.setRequestHeader("Content-type", "application/json");
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      alert("Tasting submitted succesfully");
+    }
+  };
+  let params = {
+    days: days,
+    selected: this.selected,
+  };
+
+  xmlHttp.send(JSON.stringify(params));
 }
 
 document.getElementById("saveTastings").addEventListener("click", () => {
-  this.submitTasting(
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null
-  );
+  let notes = [];
+  let guesses = [];
+  let ratings = [];
+
+  var rawNotes = document.getElementsByClassName("tastingNotes");
+  Array.prototype.forEach.call(rawNotes, function (element, index, array) {
+    notes.push(element.value);
+  });
+  var rawGuesses = document.getElementsByClassName("tastingGuess");
+  Array.prototype.forEach.call(rawGuesses, function (element, index, array) {
+    guesses.push(element.value);
+  });
+  var rawRatings = document.getElementsByClassName("tastingRating");
+  Array.prototype.forEach.call(rawRatings, function (element, index, array) {
+    ratings.push(element.value);
+  });
+
+  let days = {};
+
+  for (var i = 1; i < 13; i++) {
+    var day = {};
+    day.day = i;
+    day.note = notes.shift() ?? "";
+    day.guess = guesses.shift() ?? "";
+    day.rating = ratings.shift() ?? "";
+    days[i] = day;
+  }
+
+  this.submitTastings(days);
 });
